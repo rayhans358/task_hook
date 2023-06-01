d208dd88769cfe58288e5df8ff53b760513b39b1
rayhans358 <rayhans358@gmail.com>
2023-06-01 19:09:12 +0700

    Task Hook

---
+++ src/Task/Hook/index.jsx
@@ -0,0 +1,13 @@
+import NavbarSec from "./component/NavbarSec";
+import NewsSec from "./component/NewsSec";
+
+const Hook = () => {
+  return (
+    <div>
+      <NavbarSec />
+      <NewsSec />
+    </div>
+  )
+}
+
+export default Hook;
